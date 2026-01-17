16a61ee199359007607f5304149a0c3e8e8a167e
afiffaizin <afiffaizin758@gmail.com>
2026-01-17 14:11:30 +0700

    Update route definitions for home and dashboard views

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,18 @@ use App\Http\Controllers\ProdukController;
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('pages.home');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Admin 
-Route::get('/admin/dashboard', [ProdukController::class, 'index'])->name('admin.dashboard');
-Route::get('/admin/addProduct', [ProdukController::class, 'addProduk']);
-Route::post('/produk/store', [ProdukController::class, 'store'])->name('admin.produk.store');
-// Admin edit ProdukController
-Route::get('/admin/produk/edit/{id}', [ProdukController::class, 'edit'])->name('admin.produk.edit');
-// Admin update ProdukController
-Route::put('/admin/produk/update/{id}', [ProdukController::class, 'update'])->name('admin.produk.update');
-// Admin delete ProdukController
-Route::delete('/admin/produk/delete/{id}', [ProdukController::class, 'destroy'])->name('admin.produk.delete');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-// home
-Route::get('/', [HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
